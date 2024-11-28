9191a136ba350a89342c118e156b009f09959f01
rafaelhgreco <zrafaelgreco@gmail.com>
2024-11-28 20:57:14 -0300

    Eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      },
+      parser: tseslint.parser,
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      "@typescript-eslint/no-explicit-any": "off"
+    }
+  },
+  {
+    ignores: ["dist/", "node_modules/"]
+  }
+];
--- src/server/server.ts
+++ src/server/server.ts
@@ -2,6 +2,10 @@ import express from 'express';
 
 const server = express();
 
+interface  Teste{
+
+};
+
 server.get('/', (req, res) => {
   res.send('Hello, world!');
 });
