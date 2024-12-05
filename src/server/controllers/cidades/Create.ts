ed6c56bcbd87e28d45a67cb1cd98faa916135dda
rafaelhgreco <zrafaelgreco@gmail.com>
2024-12-05 18:40:38 -0300

    yup

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,10 +1,23 @@
 import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import * as yup from 'yup';
 interface ICidade {
     name: string;
-    state: string;
 };
 
-export const create =(req: Request<{},{},ICidade>, res: Response) => {
-    console.log(req.body);
-    res.status(200).json({ message: 'Create!' });
+const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
+    name: yup.string().required().min(3),
+});
+
+export const create = async (req: Request<{},{},ICidade>, res: Response) => {
+    let validatedAData: ICidade | undefined = undefined;  
+    try {
+        await bodyValidation.validateSync(req.body);
+    }
+    catch (error) {
+        const yupError = error as yup.ValidationError
+        res.status(StatusCodes.BAD_REQUEST).json({ message: yupError.message });
+    }
+    console.log(validatedAData);
+    
 };
