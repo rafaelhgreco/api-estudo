299817dd1f23c22ed0c6ebf048372643d1748953
rafaelhgreco <zrafaelgreco@gmail.com>
2024-12-05 22:10:26 -0300

    tratamento de erro

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -12,11 +12,16 @@ const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
 export const create = async (req: Request<{},{},ICidade>, res: Response) => {
     let validatedAData: ICidade | undefined = undefined;  
     try {
-        await bodyValidation.validateSync(req.body);
+        validatedAData = await bodyValidation.validate(req.body, { abortEarly: false });
     }
-    catch (error) {
-        const yupError = error as yup.ValidationError
-        res.status(StatusCodes.BAD_REQUEST).json({ message: yupError.message });
+    catch (err) {
+        const yupError = err as yup.ValidationError
+        const errors: Record<string, string> = {};
+        yupError.inner.forEach(error => {
+            if (!error.path) return;
+            errors[error.path!] = error.message;
+        });
+        return res.status(StatusCodes.BAD_REQUEST).json({errors});
     }
     console.log(validatedAData);
     
